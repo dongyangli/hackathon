5c0e9c96f5ce1429cdd0a0fb948c14fd75d7e59d
Dongyang Li <dongyang.li.cs@gmail.com>
2015-04-05 00:52:28 -0700

    adding in basic files

--- app.py
+++ app.py
@@ -65,11 +65,9 @@ def download():
 	filename = request.form.get("button")
 	csv = ""
 	with open(filename, 'r') as content_file:
-		csv = content_file.read()
-		"""
+		#csv = content_file.read()
 		for line in content_file:
 			csv += line + "\n"
-		"""
 	return csv
 
 # launch
